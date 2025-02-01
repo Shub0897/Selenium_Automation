2d9ea9a6fbe0f1f704a0913331ee873cd7c3f61b
tejulade <ladehtejaswini@gmail.com>
2025-02-01 10:52:56 +0530

    GitHub added

---
+++ DemoWebShop/src/test/java/GitHub/ToLearnGitHub.java
@@ -0,0 +1,13 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnGitHub {
+	@Test
+	public void learn() {
+		
+		Reporter.log("GitHub clas added",true);
+	}
+
+}
