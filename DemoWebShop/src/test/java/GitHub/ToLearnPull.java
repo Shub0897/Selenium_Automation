dd1a22602a1e10e2ec6c8393c1d60e9ba95fbd27
tejulade <ladehtejaswini@gmail.com>
2025-02-01 11:47:52 +0530

    ToLearnPull class added

---
+++ DemoWebShop/src/test/java/GitHub/ToLearnPull.java
@@ -0,0 +1,16 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnPull {
+	@Test
+	public void learn() {
+		
+		Reporter.log("ToLearnPull class added",true);
+	}
+
+}
+
+
+}
